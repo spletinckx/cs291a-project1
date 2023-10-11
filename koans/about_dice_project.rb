7e5fc677022f4ab4f80a5c2d7bc411f31b50b6d6
spletinckx <stijn9678@hotmail.com>
2023-10-11 09:10:02 -0700

    Completed

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(num_dice)
+    @values = Array.new(num_dice) { rand(1..6) }
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
